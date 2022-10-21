5d59d95d188f2be76104a5ee9220f7f9c7b48d17
Sushant Shrestha <87373725+Sushantshrestha1111@users.noreply.github.com>
2022-10-21 20:06:19 +0545

    Update Header.js

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -114,6 +114,7 @@ list-style:none;
 padding:20px;
 text-align:start;
 transform:${props => props.show ? 'translateX(0)':'translateX(100%)'};
+transition: transform 0.2s ease-in;
 li{
   padding:15px 0;
   border-bottom:1px solid rgba(0,0,0,.2);
