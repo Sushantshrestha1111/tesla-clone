309ad48f1f56f0b5543b129f814a8d5bc3199fc3
Sushant Shrestha <87373725+Sushantshrestha1111@users.noreply.github.com>
2022-10-21 11:26:17 +0545

    use state fixed

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -1,14 +1,18 @@
-import React from 'react'
+
+import React,{useState} from 'react'
 import styled from "styled-components"
 import MenuIcon from '@mui/icons-material/Menu';
-
+import Fade from 'react-reveal/Fade'
+import CloseIcon from '@mui/icons-material/Close';
 function Header() {
+  const[burgerStatus,setBurgerStatus]=useState(false);
   return (
     <Container>
         <a>
            
        
         </a>
+        
     
         <Menu>
           <a href="#">Model S</a>
@@ -20,10 +24,31 @@ function Header() {
         <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={()=>setBurgerStatus(true)} />
         
 
-        </RightMenu>
+        </RightMenu >
+
+        <BurgerMEnu show={burgerStatus}>
+          <CloseWrapper>
+          <CustomClose onClick={()=>setBurgerStatus(false)}/>
+          </CloseWrapper>
+          
+
+          <li><a herf="#">Existing</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li><li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">Existing Inventory</a></li>
+          <li><a herf="#">cyber Truck</a></li>
+        </BurgerMEnu>
     </Container>
   )
 }
@@ -39,6 +64,7 @@ padding:0 20 px;
 top:0;
 left:0;
 right:0;
+z-index:1;
 `
 
 const Menu=styled.div`
@@ -74,5 +100,37 @@ a{
 `
 const CustomMenu=styled(MenuIcon)`
 cursor:pointer;
+
+`
+const BurgerMEnu=styled.div`
+position:fixed;
+top:0;
+bottom:0;
+right:0;
+background-color:white;
+width:300px;
+z-index:16;
+list-style:none;
+padding:20px;
+text-align:start;
+transform:${props => props.show ? 'translateX(0)':'translateX(100%)'};
+li{
+  padding:15px 0;
+  border-bottom:1px solid rgba(0,0,0,.2);
+  a{
+    font-weight:650;
+
+  }
+}
+
+
+`
+const CustomClose=styled(CloseIcon)`
+cursor:pointer;
+
+`
+const CloseWrapper=styled.div`
+display:flex;
+justify-content:flex-end;
 
 `
